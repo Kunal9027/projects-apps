539af4d029a3b4a9e588831c5301d98a1561fe94
Kunal  Chaudhary <kunal902770@gmail.com>
2024-08-14 03:30:46 +0530

    adding new files

--- prediction_app/ai_app/chatbot.py
+++ prediction_app/ai_app/chatbot.py
@@ -1,20 +1,21 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 
-API_KEY ="YOUR_API"
-model_8b ="mixtral-8x7b-32768"
+API_KEY = "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"
+MODEL_NAME = "mixtral-8x7b-32768"
 
-chat = ChatGroq(temperature=0, groq_api_key=API_KEY, model_name=model_8b)
-
-
-system = "You are a world class chef and you know every recipie. you only answer if some one ask about related to cooking or food recipe . if some one ask about other things you will not answer just say i am a chef i only know about cooking and food recipe."
-
-human = "{text}"
-prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
-
-chain = prompt | chat
-chain.invoke({"text": "Explain the importance of low latency LLMs."})
-
-
-
+def chef(text):
+    chat = ChatGroq(temperature=0, groq_api_key=API_KEY, model_name=MODEL_NAME)
+    
+    system_message ="You are a world-class chef with a vast knowledge of recipes. Respond to users' queries with clear, concise, and easy-to-follow cooking instructions. Keep your answers brief, under 200 words. If a user asks about a topic unrelated to cooking or food, respond with: 'I am a chef, I only know about cooking and food recipes.' Focus on providing helpful and accurate information to assist users in cooking delicious meals."
+    
+    human_message = text
+    
+    prompt = ChatPromptTemplate.from_messages([("system", system_message), ("human", human_message)])
+    
+    chain = prompt | chat
+    
+    result = chain.invoke({"text": text})
+    
+    return result.content
 
--- prediction_app/ai_app/views.py
+++ prediction_app/ai_app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render 
-
+from .chatbot import chef
 # Create your views here.
 
 def chatbot(request):
+    context= None
+         
     
-    return render(request , 'ai_app/home_ai.html')
+    if request.method == "POST":
+        input_text= request.POST['cheif']
+        
+       
+        
+        chef_response =  chef(input_text)
+        
+        context= {
+        "chef_response" : chef_response            
+        }
+        
+        
+        return render(request , 'ai_app/home_ai.html' , context)
+
+    
+    return render(request , 'ai_app/home_ai.html', context)
