44abd61ead56313f8c68a83ad65463da49bbd14d
Kunal  Chaudhary <kunal902770@gmail.com>
2025-02-16 16:30:05 +0530

    add

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+# using build.sh to collect static files on render beacuse it is not possible to run collectstatic command directly on render free tier this file help to run collectstatic command on render free tier 
+
 #!/usr/bin/env bash
 python manage.py collectstatic --noinput
