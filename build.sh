9d9e5d0cf9848573849ea761c0468a8b20c512e4
Kunal  Chaudhary <kunal902770@gmail.com>
2025-02-23 20:58:29 +0530

    update

--- build.sh
+++ build.sh
@@ -5,11 +5,5 @@
 # Exit on error
 set -o errexit
 
-# Install Python dependencies
-pip install -r requirements.txt
-
 # Collect static files
 python manage.py collectstatic --no-input
-
-# Run migrations
-python manage.py migrate
